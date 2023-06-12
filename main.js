52d6fefeabd4bd0a999185139b195d7461347320
Alexander <losev.pavlovo@mail.ru>
2023-06-12 17:34:50 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// const age = 15;
+// const nameUser = "Oven";
+
+// if (age > 14) {
+//   alert("Sorry");
+// } else {
+//   alert("welocome" + nameUser + "!");
+// }
+
+const winner = 2;
+const name = "Alex";
+const isEligible = false;
